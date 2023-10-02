73aeb3568dbe814b832e1415ba565e57dc78c844
Christopher Arthaud <christopher.arthaud@gmail.com>
2023-10-03 01:36:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+console.log(chalk.blue('Cricri'));
+
+console.log(chalk.green('Gaetan'));
+
+console.log(chalk.bold.rgb(255, 147, 216)('Messaoud'));
+
+console.log(chalk.red('Nass'));
